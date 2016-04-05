20b75336d11fb21c739bc3001f7ca972f9dec5cc
Despoina Karna <falsedk@hotmail.com>
2016-04-05 18:13:43 +0100

    added prerequisites script Initial commit

---
+++ prerequisites.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo check os version
+cat /etc/issue
+
+echo setup passwordless ssh login
+
+echo save the existing firewall configurations
+sudo iptables-save > /etc/sysconfig/iptables
+echo disable firewall
+sudo service iptables stop
+echo Validate if firewall is disabled or not using the following command
+sudo service iptables status
+
+echo disble SELInux
+sudo /usr/sbin/setenforce 0
+sudo sed -i.old s/SELINUX=enforcing/SELINUX=disabled/ /etc/selinux/config
+echo check status
+sestatus
+
+
+
