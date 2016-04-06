1f23dc623f58edbbec4d9c52f8ebe9f406aeb085
taffazzel <tafaz_hossain@yahoo.com>
2016-04-06 03:25:36 +0100

    Update ambari-server.sh

--- ambari-server.sh
+++ ambari-server.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 cd /etc/yum.repos.d/
+rm /etc/yum.repos.d/ambari.repo
 wget http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.2.1.0/ambari.repo
 yum install ambari-server
 ambari-server setup -j ambari-server setup -j /usr/java/latest/
