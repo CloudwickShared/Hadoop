2097266025673d65d7b570038d476e997935b7ed
Derwin McGeary <derwinmcgeary+web@gmail.com>
2016-04-10 12:16:03 +0100

    Fix bug where #! was not line one

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -1,4 +1,3 @@
-## the code in the git library 
 #!/bin/bash
 # Simple setup.sh for configuring CentOS6 and Hadoop
 yum -y install wget
@@ -56,8 +55,8 @@ if grep -q "echo never > /sys/kernel/mm/transparent_hugepage/enabled" /etc/rc.lo
 fi
 
 
-cat hosts.list>> /etc/hosts
-echo "Update the /etc/hosts"
+# cat hosts.list>> /etc/hosts
+# echo "Update the /etc/hosts"
 
 cd /opt
 
@@ -80,6 +79,10 @@ echo "installed mysql connector to java"
 yum -y install ambari-agent
 
 
-echo "ambari is installed"
+echo "ambari-agent is installed"
+
+sed -i.old -e 's/hostname=localhost/hostname=ambari.cloudwick.com/' /etc/ambari-agent/conf/ambari-agent.ini
+
+echo "ambari-agent is configured"
 
 echo " prerequisites are installed" 
