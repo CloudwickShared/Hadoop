349f88769e7e4915053f4e7d4fa8aec601931fcb
Derwin McGeary <derwinmcgeary+web@gmail.com>
2016-04-25 17:26:25 +0100

    Added noclobber so that download can be resumed later

--- scripts/downloadairlinedata.sh
+++ scripts/downloadairlinedata.sh
@@ -7,12 +7,10 @@ do
     for month in {1..12}
     do
 	echo $year _ $month
-	wget http://www.transtats.bts.gov/Download/On_Time_On_Time_Performance_"$year"_"$month".zip
+	wget -nc http://www.transtats.bts.gov/Download/On_Time_On_Time_Performance_"$year"_"$month".zip
 	sleep 5
     done
 done
 cd ..
-
-
 
 
